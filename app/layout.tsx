610ea1873df78dd2cf0910380c4d69bd9323e898
njugunabrain301 <48784973+njugunabrain301@users.noreply.github.com>
2025-09-16 21:26:09 +0300

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GlobalContextProvider } from "@/Context/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <GlobalContextProvider
+        profile={null}
+        titleFont={null}
+        bodyFont={null}
+        subtitleFont={null}
+        checkoutInfo={null}
+      >
+        <body className={inter.className}>{children}</body>
+      </GlobalContextProvider>
     </html>
   );
 }
